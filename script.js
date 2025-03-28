44acdb6731715fe272f03081a552dae0ca53d2a1
ditteruu <droquetercero@gmail.com>
2025-03-28 16:30:32 -0600

    inicios js

---
+++ script.js
@@ -0,0 +1,4 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".bill-button");
+const tipPerperson = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("total-amount");
